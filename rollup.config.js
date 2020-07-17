0fd01cc7083d565a704729605821d741d5d19f6e
Edgar Albalate <dev.gardo@gmail.com>
2020-07-17 17:51:16 +0200

    Updated some dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import svelte from 'rollup-plugin-svelte';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import rollup_start_dev from './rollup_start_dev';
@@ -30,7 +30,7 @@ export default {
 		// npm, you'll most likely need these plugins. In
 		// some cases you'll need additional configuration —
 		// consult the documentation for details:
-		// https://github.com/rollup/rollup-plugin-commonjs
+		// https://github.com/rollup/@rollup/plugin-commonjs
 		resolve({
 			browser: true,
 			dedupe: importee => importee === 'svelte' || importee.startsWith('svelte/')
